5d40e8005a1b14c658822c2d9fb9258d8c7d5392
Jesse Theisen <jesse.theisen@snhu.edu>
2023-01-08 23:12:44 -0600

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+/*
+Name: Jesse Theisen
+Profressor: Michael Rissover
+Description: displays a 12 hour in 00:00:00 format, user can enter a custom time but doesn't display current time
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int hour;
+	int minute;
+	int second;
+	int totalTime;
+
+	cin >> hour;
+	cin >> minute;
+	cin >> second;
+
+	cout << "The current time is: ";
+
+
+	// add for loop for hours minutes and seconds
+
+
+}
